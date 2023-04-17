90d8a8741ab071d38050bf9a3d28c08668034a05
alex-spaero <109698795+alex-spaero@users.noreply.github.com>
2023-04-17 12:57:33 -0700

    Remove logging

--- pylabrobot/__init__.py
+++ pylabrobot/__init__.py
@@ -7,8 +7,8 @@ from pylabrobot.__version__ import __version__
 logger = logging.getLogger(__name__)
 
 # Add a file handler
-now = datetime.datetime.now().strftime("%Y%m%d")
-fh = logging.FileHandler(f"pylabrobot-{now}.log")
-fh.setLevel(logging.DEBUG)
-fh.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
-logger.addHandler(fh)
+# now = datetime.datetime.now().strftime("%Y%m%d")
+# fh = logging.FileHandler(f"pylabrobot-{now}.log")
+# fh.setLevel(logging.DEBUG)
+# fh.setFormatter(logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s"))
+# logger.addHandler(fh)
